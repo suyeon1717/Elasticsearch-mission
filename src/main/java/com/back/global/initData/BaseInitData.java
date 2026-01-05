63b6f3be9ba4ed5bf3479fc1fee841cc12ddd5d2
sy <a123@Mac-Pro.local>
2026-01-05 16:32:38 +0900

    0013 - Post 단건 조회 기능 구현 (Read - Single)

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -19,6 +19,7 @@ public class BaseInitData {
         return args->{
             work1();
             work2();
+            work3();
         };
     }
 
@@ -40,6 +41,13 @@ public class BaseInitData {
         log.debug("기존 Post 전체 조회");
         for (Post post : postService.findAll()) {
             log.debug("Existing Post: {}", post);
+        }
+    }
+
+    private void work3() {
+        log.debug("Post 단건 조회");
+        for (Post post : postService.findAll()) {
+            log.debug("조회된 Post: {}", post);
         }
     }
 }
